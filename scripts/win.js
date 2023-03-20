8eb030deedf34e728e8f28be7344baa91b6eb87c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-20 08:33:46 +0000

    🔧 chore(deploy.js): change contractName variable from "Game1" to "Game2" 🔧 chore(win.js): add game address and update contractName variable from "Game1" to "Game2"

--- scripts/win.js
+++ scripts/win.js
@@ -1,5 +1,5 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
+const gameAddr = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const contractName = "Game1";
 
 async function main() {
