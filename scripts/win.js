f79f64dd715dbc663a13444a47f9adeacd5d6f86
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-20 09:11:37 +0000

    🐛 fix(deploy.js): change contractName from Game4 to Game5 ✨ feat(win.js): add minting and allowance functionality before winning the game. Update contract address and contractName from Game4 to Game5.

--- scripts/win.js
+++ scripts/win.js
@@ -1,14 +1,19 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
-const contractName = "Game4";
+const gameAddr = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
+    let amount = 10000
+    const allowance = await game.giveMeAllowance(amount);
+    await allowance.wait();
+    const minter = await game.mint(amount);
+    await minter.wait()
 
-    const tx = await game.win(56);
+    const tx = await game.win();
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
