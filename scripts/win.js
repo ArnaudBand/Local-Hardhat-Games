4beca112316e83e828e488fc68179f5547ecdb4b
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-20 08:41:25 +0000

    🎮 chore(win.js): update game address and contract name 🚀 feat(win.js): add code to set x and y values before winning the game

--- scripts/win.js
+++ scripts/win.js
@@ -1,12 +1,17 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const contractName = "Game1";
+const gameAddr = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
+
+    const x = await game.setX(10);
+    await x.wait();
+    const y = await game.setY(40);
+    await y.wait();
     const tx = await game.win();
 
     // did you win? Check the transaction receipt!
